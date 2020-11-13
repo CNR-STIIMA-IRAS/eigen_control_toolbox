81e58632b1cf11816540a1a4f50dc1572fe3787b
nicola pedrocchi <nicola.pedrocchi@stiima.cnr.it>
2020-11-13 12:07:17 +0100

    added travis for CI/CD

--- .codecov.sh
+++ .codecov.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Export CI environment info
+export CODECOV_TOKEN=5de39e6d-c202-42e7-84b1-961bd00650e1
 ci_env=$(bash <(curl -s https://codecov.io/env))
 export DOCKER_RUN_OPTS="$DOCKER_RUN_OPTS $ci_env"
