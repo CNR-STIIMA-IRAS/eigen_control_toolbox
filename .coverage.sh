949d8830e2731f42819a5699aee664849641aad5
nicola pedrocchi <nicola.pedrocchi@stiima.cnr.it>
2020-11-19 10:26:11 +0100

    travis

--- .coverage.sh
+++ .coverage.sh
@@ -3,7 +3,7 @@
 source "${ICI_SRC_PATH}/workspace.sh"
 source "${ICI_SRC_PATH}/util.sh"
 
-echo "Generating coverage for cnr_logger"
+echo "Generating coverage for eigen_state_space_systems"
 
 ws=~/target_ws
 extend="/opt/ros/$ROS_DISTRO"
@@ -12,8 +12,8 @@ ici_exec_in_workspace "$extend" "$ws" catkin build cnr_logger -v --no-deps --cat
 echo "Uploading coverage results to codecov.io"
 
 # Remove duplicated information
-rm "$ws/build/cnr_logger/cnr_logger_coverage_report.info.cleaned"
-rm "$ws/build/cnr_logger/cnr_logger_coverage_report.info.removed"
+rm "$ws/build/eigen_state_space_systems/coverage_report.info.cleaned"
+rm "$ws/build/eigen_state_space_systems/coverage_report.info.removed"
 
 # Actually upload coverage information
-bash <(curl -s https://codecov.io/bash) -s "$ws/build/cnr_logger/"
+bash <(curl -s https://codecov.io/bash) -s "$ws/build/eigen_state_space_systems/"
